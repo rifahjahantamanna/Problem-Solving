9a32f76a349882cf010e65f0e7fdc4607dc3b088
Rifah <rifahjahan2000@gmail.com>
2026-07-12 17:06:22 +0600

    Added Solution

---
+++ AtCoder/week_records.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n*7];
+    for(int i=0; i<n*7; ++i)
+    {
+        scanf("%d", &arr[i]);
+    };
+    for (int week=1; week<=n; ++week)
+    { 
+        int ans_for_this_week=0;
+        int start=(week-1)*7;
+        for (int i=0; i<7; ++i)
+        {
+            ans_for_this_week+=arr[start+i];
+        }
+        printf("%d ",ans_for_this_week);
+    }
+
+
+    return 0;
+}
